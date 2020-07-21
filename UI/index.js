db8242972f181585afec4e7a72650ce9a4df82ce
sarojshakya01 <sarojsh01@gmail.com>
2020-07-21 14:48:07 +0545

    minor changes

---
+++ UI/index.js
@@ -0,0 +1,16 @@
+addEventListener(
+  "load",
+  function () {
+    setTimeout(hideURLbar, 0);
+  },
+  false
+);
+function hideURLbar() {
+  window.scrollTo(0, 1);
+}
+
+// var totGallon = document.getElementById("gallonreq");
+
+// var price = document.getElementById("sugprice");
+
+// document.getElementById("deuamount").value = 0 || totGallon * price;
